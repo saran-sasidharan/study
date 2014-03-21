fc54cb459e423384084f05582d85e818fa12790e
saran-sasidharan <saran.s.dhar@gmail.com>
2014-03-21 23:56:26 +0530

    Better parallelization

--- pif.c
+++ pif.c
@@ -12,20 +12,26 @@ int main()
 	int tot_threads = omp_get_num_threads();
 	long new_steps = num_steps/tot_threads; // assuming even threads always, preferably 4
 
-	int i; double pi; double sum[tot_threads] = {0.0};
+	double pi; 
+	double sum[tot_threads];
 	
 	step = 1.0/(double)num_steps;
 
 	#pragma omp parallel
 	{
 	double x;
-	int rank = omp_get_thread_num();
+	int i,rank = omp_get_thread_num();
+	//sum[rank] = 0.0;
+	double temp=0.0;
 	for(i=0;i<new_steps;i++){
 		x = ((i+rank*new_steps)+0.5)*step;
-		sum[rank] = sum[rank] + 4.0/(1.0+x*x);
+	//	sum[rank] = sum[rank] + 4.0/(1.0+x*x);
+		temp = temp + 4.0/(1.0+x*x);
 	}
+	sum[rank] = temp;
 	}
 	double tot_sum = 0.0;
+	int i;
 	for(i=0;i<tot_threads;i++){
 		tot_sum = tot_sum + sum[i];
 	}
